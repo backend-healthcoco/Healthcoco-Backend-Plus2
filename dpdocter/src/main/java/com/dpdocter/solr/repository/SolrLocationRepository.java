c9f43d878d2652391bbb029735162bd7c0aa7569
isank <isankagarwal@gmail.com>
2015-11-29 16:06:20 +0530

    Minor Changes to Appointment

--- dpdocter/src/main/java/com/dpdocter/solr/repository/SolrLocationRepository.java
+++ dpdocter/src/main/java/com/dpdocter/solr/repository/SolrLocationRepository.java
@@ -8,6 +8,9 @@ import org.springframework.data.solr.repository.SolrCrudRepository;
 import com.dpdocter.solr.document.SolrLocationDocument;
 
 public interface SolrLocationRepository extends SolrCrudRepository<SolrLocationDocument, String> {
-    @Query("locationName : *?0* OR landmarkDetails : *?0* OR locality : *?0* AND city : *?1*")
+    @Query("(locationName : *?0* OR landmarkDetails : *?0* OR locality : *?0*) AND city : *?1*")
     List<SolrLocationDocument> findAll(String location, String city);
+
+    @Query("city : *?0*")
+    List<SolrLocationDocument> findAll(String city);
 }
--- dpdocter/src/main/java/com/dpdocter/tests/GeneralTests.java
+++ dpdocter/src/main/java/com/dpdocter/tests/GeneralTests.java
@@ -1,19 +1,15 @@
 package com.dpdocter.tests;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.dpdocter.solr.document.SolrSpecialityDocument;
+import com.dpdocter.beans.Landmark;
 
 public class GeneralTests {
     public static void main(String args[]) {
-	String[] specialities = { "Cold", "Cough", "Fever" };
-	List<SolrSpecialityDocument> solrSpecialityDocuments = new ArrayList<SolrSpecialityDocument>();
-	for (String speciality : specialities) {
-	    SolrSpecialityDocument solrSpecialityDocument = new SolrSpecialityDocument();
-	    solrSpecialityDocument.setSpeciality(speciality);
-	    solrSpecialityDocuments.add(solrSpecialityDocument);
-	}
-	System.out.println(Converter.ObjectToJSON(solrSpecialityDocuments));
+	Landmark landmark = new Landmark();
+
+	landmark.setCityId("565ac97127360c89a4c99d86");
+	landmark.setDescription("Test Landmark");
+	landmark.setLandmark("Panchsheel");
+
+	System.out.println(Converter.ObjectToJSON(landmark));
     }
 }
