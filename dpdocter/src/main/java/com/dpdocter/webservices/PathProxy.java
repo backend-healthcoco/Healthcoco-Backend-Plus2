c7a1dc49d61dff250c1db98d17053bd419690fe9
Parag Pakhale <parag.pakhale@healthcoco.com>
2016-09-02 13:17:14 +0530

    Added url in path proxy and configured web service in sign up api for clinic contact us functionality

--- dpdocter/src/main/java/com/dpdocter/webservices/PathProxy.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/PathProxy.java
@@ -33,6 +33,8 @@ public interface PathProxy {
 		
 		public static final String SUBMIT_DOCTOR_CONTACT = "/submitDoctorContact";
 		
+		public static final String SUBMIT_CLINIC_CONTACT = "/submitClinicContact";
+		
 		public static final String RESEND_VERIFICATION_EMAIL_TO_DOCTOR = "/resendVerificationEmail/{emailaddress}";
 
 	}
--- dpdocter/src/main/java/com/dpdocter/webservices/SignUpApi.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/SignUpApi.java
@@ -273,7 +273,7 @@ public class SignUpApi {
     	return response;
     }
     
-    /*@Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     @Path(value = PathProxy.SignUpUrls.SUBMIT_CLINIC_CONTACT)
     @POST
     @ApiOperation(value = PathProxy.SignUpUrls.SUBMIT_CLINIC_CONTACT, notes = PathProxy.SignUpUrls.SUBMIT_CLINIC_CONTACT)
@@ -282,17 +282,17 @@ public class SignUpApi {
     	if(clinicContactUs == null){
     		logger.warn("Clinic contact data is null");
     	    throw new BusinessException(ServiceError.InvalidInput, "Clinic Contact data is null");
-    	}else if(DPDoctorUtils.anyStringEmpty(clinicContactUs.getl, doctorContactUs.getEmailAddress(), doctorContactUs.getTitle(),doctorContactUs.getCity(), doctorContactUs.getMobileNumber()) || doctorContactUs.getGender() == null || doctorContactUs.getSpecialities() == null || doctorContactUs.getSpecialities().isEmpty()){
+    	}/*else if(DPDoctorUtils.anyStringEmpty(clinicContactUs.get, doctorContactUs.getEmailAddress(), doctorContactUs.getTitle(),doctorContactUs.getCity(), doctorContactUs.getMobileNumber()) || doctorContactUs.getGender() == null || doctorContactUs.getSpecialities() == null || doctorContactUs.getSpecialities().isEmpty()){
     		logger.warn("Invalid Input");
     		throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
     	}else if (doctorContactUs.getFirstName().length() < 2) {
     		logger.warn(firstNameValidaton);
     		throw new BusinessException(ServiceError.InvalidInput, firstNameValidaton);
-    	 }
+    	 }*/
     	
     	Response<String> response = new Response<String>();
     	response.setData(clinicContactUsService.submitClinicContactUSInfo(clinicContactUs));
     	return response;
-    }*/
+    }
     
 }
