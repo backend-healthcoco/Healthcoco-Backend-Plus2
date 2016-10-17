3ffff4bf3a05161831a2a204143ff8c5c57e1a7b
Parag Pakhale <parag.pakhale@healthcoco.com>
2016-10-17 19:17:26 +0530

    Updated dynamic UI service

--- dpdocter/src/main/java/com/dpdocter/services/impl/DynamicUIServiceImpl.java
+++ dpdocter/src/main/java/com/dpdocter/services/impl/DynamicUIServiceImpl.java
@@ -3,18 +3,23 @@ package com.dpdocter.services.impl;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dpdocter.beans.DynamicUIRequest;
 import com.dpdocter.beans.UIPermissions;
+import com.dpdocter.collections.UserCollection;
 import com.dpdocter.enums.ClinicalNotesPermissionEnum;
 import com.dpdocter.enums.DynamicUIEnum;
+import com.dpdocter.enums.GynacPermissionsEnum;
 import com.dpdocter.enums.HistoryPermissionEnum;
+import com.dpdocter.enums.OpthoPermissionEnums;
 import com.dpdocter.enums.PrescriptionPermissionEnum;
 import com.dpdocter.enums.SpecialityTypeEnum;
 import com.dpdocter.repository.DynamicUIRepository;
+import com.dpdocter.repository.UserRepository;
 import com.dpdocter.services.DynamicUIService;
 @Service
 public class DynamicUIServiceImpl implements DynamicUIService{
@@ -22,10 +27,15 @@ public class DynamicUIServiceImpl implements DynamicUIService{
 	@Autowired
 	DynamicUIRepository dynamicUIRepository;
 	
+	@Autowired
+	UserRepository userRepository;
+	
 	@Transactional
 	public UIPermissions getAllPermissionForDoctor(String doctorId)
 	{
-		return null;
+		UIPermissions uiPermissions = null;
+		UserCollection userCollection = userRepository.findOne(new ObjectId(doctorId));
+		return uiPermissions;
 	}
 	
 	@Transactional
@@ -50,13 +60,34 @@ public class DynamicUIServiceImpl implements DynamicUIService{
 		case OPHTHALMOLOGIST:
 			uiPermissions = new UIPermissions();
 			clinicalNotesPermission = new ArrayList<String>(Arrays.asList(clinicalNotesPermission()));
+			clinicalNotesPermission.add(OpthoPermissionEnums.OPTHO_CLINICAL_NOTES.getPermissions());
 			prescriptionPermission = new ArrayList<String>(Arrays.asList(prescriptionPermission()));
+			prescriptionPermission.add(OpthoPermissionEnums.OPTHO_RX.getPermissions());
 			historyPermission = new ArrayList<String>(Arrays.asList(historyPermission()));
 			uiPermissions.setClinicalNotesPermissions(clinicalNotesPermission);
 			uiPermissions.setPrescriptionPermissions(prescriptionPermission);
 			uiPermissions.setHistoryPermissions(historyPermission);
 			break;
-
+		case PEDIATRICIAN:
+			uiPermissions = new UIPermissions();
+			clinicalNotesPermission = new ArrayList<String>(Arrays.asList(clinicalNotesPermission()));
+			prescriptionPermission = new ArrayList<String>(Arrays.asList(prescriptionPermission()));
+			historyPermission = new ArrayList<String>(Arrays.asList(historyPermission()));
+			historyPermission.add(GynacPermissionsEnum.BIRTH_HISTORY.getPermissions());
+			uiPermissions.setClinicalNotesPermissions(clinicalNotesPermission);
+			uiPermissions.setPrescriptionPermissions(prescriptionPermission);
+			uiPermissions.setHistoryPermissions(historyPermission);
+			break;
+		case GYNAECOLOGIST:
+			uiPermissions = new UIPermissions();
+			clinicalNotesPermission = new ArrayList<String>(Arrays.asList(clinicalNotesPermission()));
+			prescriptionPermission = new ArrayList<String>(Arrays.asList(prescriptionPermission()));
+			historyPermission = new ArrayList<String>(Arrays.asList(historyPermission()));
+			historyPermission.add(GynacPermissionsEnum.BIRTH_HISTORY.getPermissions());
+			uiPermissions.setClinicalNotesPermissions(clinicalNotesPermission);
+			uiPermissions.setPrescriptionPermissions(prescriptionPermission);
+			uiPermissions.setHistoryPermissions(historyPermission);
+			break;
 		default:
 			uiPermissions = new UIPermissions();
 			clinicalNotesPermission = new ArrayList<String>(Arrays.asList(clinicalNotesPermission()));
