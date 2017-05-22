60c3a00318b048b7df1e0f5fc6e24d1ce50bc1f8
Parag Pakhale <parag.pakhale@healthcoco.com>
2017-05-22 13:16:00 +0530

    updated path proxy

--- dpdocter/src/main/java/com/dpdocter/webservices/LabApi.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/LabApi.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,14 +13,19 @@ import javax.ws.rs.core.MediaType;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.dpdocter.beans.Clinic;
+import com.dpdocter.beans.CollectionBoy;
+import com.dpdocter.beans.LabTestPickup;
+import com.dpdocter.beans.RateCard;
+import com.dpdocter.beans.RateCardTestAssociation;
 import com.dpdocter.beans.Records;
 import com.dpdocter.exceptions.BusinessException;
 import com.dpdocter.exceptions.ServiceError;
+import com.dpdocter.request.AddEditLabTestPickupRequest;
 import com.dpdocter.services.LabService;
+import com.dpdocter.services.LocationServices;
 
 import common.util.web.DPDoctorUtils;
 import common.util.web.Response;
@@ -37,6 +43,9 @@ public class LabApi {
 
 	@Autowired
 	private LabService labService;
+	
+	@Autowired
+	private LocationServices locationServices;
 
 	@Path(value = PathProxy.LabUrls.GET_CLINICS_WITH_REPORTS_COUNT)
 	@GET
@@ -73,5 +82,90 @@ public class LabApi {
 
 		return response;
 	}
+
+	@Path(value = PathProxy.LabUrls.ADD_EDIT_PICKUP_REQUEST)
+	@POST
+	@ApiOperation(value = PathProxy.LabUrls.ADD_EDIT_PICKUP_REQUEST, notes = PathProxy.LabUrls.ADD_EDIT_PICKUP_REQUEST)
+	public Response<LabTestPickup> addEditPickupRequest(AddEditLabTestPickupRequest request) {
+		if (request == null) {
+			logger.warn("Invalid Input");
+			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
+		}
+		Response<LabTestPickup> response = new Response<LabTestPickup>();
+		response.setData(locationServices.addEditLabTestPickupRequest(request));
+
+		return response;
+	}
+	
+	@Path(value = PathProxy.LabUrls.GET_CB_LIST_BY_PARENT_LAB)
+	@GET
+	@ApiOperation(value = PathProxy.LabUrls.GET_CB_LIST_BY_PARENT_LAB, notes = PathProxy.LabUrls.GET_CB_LIST_BY_PARENT_LAB)
+	public Response<CollectionBoy> getCBListByParentLab(@QueryParam("locationId") String locationId , @QueryParam("page") int page, @QueryParam("size") int size, @QueryParam("searchTerm") String searchTerm) {
+		if (locationId == null) {
+			logger.warn("Invalid Input");
+			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
+		}
+		Response<CollectionBoy> response = new Response<CollectionBoy>();
+		response.setDataList(locationServices.getCollectionBoyList(size, page, locationId, searchTerm));
+
+		return response;
+	}
+	
+
+	@Path(value = PathProxy.LabUrls.GET_RATE_CARDS)
+	@GET
+	@ApiOperation(value = PathProxy.LabUrls.GET_RATE_CARDS, notes = PathProxy.LabUrls.GET_RATE_CARDS)
+	public Response<RateCard> getRateCards(@QueryParam("locationId") String locationId , @QueryParam("page") int page, @QueryParam("size") int size, @QueryParam("searchTerm") String searchTerm) {
+		if (locationId == null) {
+			logger.warn("Invalid Input");
+			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
+		}
+		Response<RateCard> response = new Response<RateCard>();
+		response.setDataList(locationServices.getRateCards(page, size, searchTerm, locationId));
+
+		return response;
+	}
+	
+	@Path(value = PathProxy.LabUrls.GET_RATE_CARD_TEST)
+	@GET
+	@ApiOperation(value = PathProxy.LabUrls.GET_RATE_CARD_TEST, notes = PathProxy.LabUrls.GET_RATE_CARD_TEST)
+	public Response<RateCardTestAssociation> getRateCardTests(@QueryParam("rateCardId") String rateCardId , @QueryParam("page") int page, @QueryParam("size") int size, @QueryParam("labId") String labId, @QueryParam("searchTerm") String searchTerm) {
+		if (rateCardId == null) {
+			logger.warn("Invalid Input");
+			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
+		}
+		Response<RateCardTestAssociation> response = new Response<RateCardTestAssociation>();
+		response.setDataList(locationServices.getRateCardTests(page, size, searchTerm, rateCardId, labId));
+
+		return response;
+	}
+	
+	@Path(value = PathProxy.LabUrls.ADD_EDIT_RATE_CARD)
+	@POST
+	@ApiOperation(value = PathProxy.LabUrls.ADD_EDIT_RATE_CARD, notes = PathProxy.LabUrls.ADD_EDIT_RATE_CARD)
+	public Response<RateCard> addEditRateCard(RateCard request) {
+		if (request == null) {
+			logger.warn("Invalid Input");
+			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
+		}
+		Response<RateCard> response = new Response<RateCard>();
+		response.setData(locationServices.addEditRateCard(request));
+		return response;
+	}
+	
+	@Path(value = PathProxy.LabUrls.ADD_EDIT_RATE_CARD_TESTS)
+	@POST
+	@ApiOperation(value = PathProxy.LabUrls.ADD_EDIT_RATE_CARD_TESTS, notes = PathProxy.LabUrls.ADD_EDIT_RATE_CARD_TESTS)
+	public Response<RateCardTestAssociation> addEditRateCardTests(RateCardTestAssociation request) {
+		if (request == null) {
+			logger.warn("Invalid Input");
+			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
+		}
+		Response<RateCardTestAssociation> response = new Response<RateCardTestAssociation>();
+		response.setData(locationServices.addEditRateCardTestAssociation(request));
+		return response;
+	}
+	
+	
 
 }
--- dpdocter/src/main/java/com/dpdocter/webservices/PathProxy.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/PathProxy.java
@@ -1126,6 +1126,19 @@ public interface PathProxy {
 	public interface LabUrls {
 		public static final String GET_CLINICS_WITH_REPORTS_COUNT = "/getClinicWithReportCount/{doctorId}/{locationId}/{hospitalId}";
 		public static final String GET_REPORTS_FOR_SPECIFIC_DOCTOR = "/getReports/{doctorId}/{locationId}/{hospitalId}";
+		public static final String ADD_EDIT_PICKUP_REQUEST = "/addEditPickupRequest";
+		public static final String GET_COLLECTION_BOY_LIST = "/getCollectionBoys";
+		public static final String GET_CB_LIST_BY_PARENT_LAB = "/getCBListByParentLab";
+		public static final String GET_DAUGHTER_LAB_LIST_BY_CB = "/getDaughterListByCB";
+		public static final String CB_LAB_ASSOCIATION = "/cbLabAssociation";
+		public static final String ADD_EDIT_COLLECTION_BOY = "/addEditCollectionBoy";
+		public static final String DISCARD_COLLECTIONe_BOY = "/discardCollectionBoy";
+		public static final String VERIFY_CRN = "/verifyCRN";
+		public static final String GET_RATE_CARDS = "/getRateCards";
+		public static final String GET_RATE_CARD_TEST = "/getRateCardTests";
+		public static final String ADD_EDIT_RATE_CARD = "/addEditRateCards";
+		public static final String ADD_EDIT_RATE_CARD_TESTS	= "addEditRateCardTests";
+		
 	}
 
 }
