e7dfe7d0d6ee5740ff0fa6fb5b1f3239a058ab03
Neha Pateliya <neha.pateliya@healthcoco.com>
2017-09-09 22:42:19 -0400

    -Revert appointment slot code

--- dpdocter/src/main/java/com/dpdocter/repository/AppointmentBookedSlotRepository.java
+++ dpdocter/src/main/java/com/dpdocter/repository/AppointmentBookedSlotRepository.java
@@ -13,7 +13,7 @@ import com.dpdocter.collections.AppointmentBookedSlotCollection;
 public interface AppointmentBookedSlotRepository extends MongoRepository<AppointmentBookedSlotCollection, ObjectId> {
 
 	@Query("{'doctorId': ?0, 'locationId': ?1, 'fromDate': {'$gte':?2}, 'toDate': {'$lte':?3}}")
-    List<AppointmentBookedSlotCollection> findByDoctorLocationId(ObjectId doctorId, ObjectId locationId, DateTime start ,DateTime end, Sort sort);
+    List<AppointmentBookedSlotCollection> findByDoctorLocationId(ObjectId doctorId, ObjectId locationId, DateTime start ,DateTime end);
 
     @Query("{'appointmentId': ?0}")
     AppointmentBookedSlotCollection findByAppointmentId(String appointmentId);
