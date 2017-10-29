5cd3da6c6cc877f204d700f717bf6376f57ca1d0
Neha Pateliya <neha.pateliya@healthcoco.com>
2017-10-29 02:35:19 -0400

    -Added LabSearchResponse file

---
+++ dpdocter/src/main/java/com/dpdocter/response/LabSearchResponse.java
@@ -0,0 +1,87 @@
+package com.dpdocter.response;
+
+import java.util.List;
+
+import com.dpdocter.beans.DiagnosticTest;
+
+public class LabSearchResponse {
+
+	private String id;
+	
+	private String locationName;
+	
+	private Boolean isNABLAccredited = false;
+
+	List<DiagnosticTest> diagnosticTests;
+	
+	private Double totalCost = 0.0;
+
+    private Double totalCostForPatient = 0.0;
+
+    private Double totalSavingInPercentage = 0.0;
+    
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLocationName() {
+		return locationName;
+	}
+
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
+
+	public Boolean getIsNABLAccredited() {
+		return isNABLAccredited;
+	}
+
+	public void setIsNABLAccredited(Boolean isNABLAccredited) {
+		this.isNABLAccredited = isNABLAccredited;
+	}
+
+	public List<DiagnosticTest> getDiagnosticTests() {
+		return diagnosticTests;
+	}
+
+	public void setDiagnosticTests(List<DiagnosticTest> diagnosticTests) {
+		this.diagnosticTests = diagnosticTests;
+	}
+
+	public Double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(Double totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	public Double getTotalCostForPatient() {
+		return totalCostForPatient;
+	}
+
+	public void setTotalCostForPatient(Double totalCostForPatient) {
+		this.totalCostForPatient = totalCostForPatient;
+	}
+
+	public Double getTotalSavingInPercentage() {
+		return totalSavingInPercentage;
+	}
+
+	public void setTotalSavingInPercentage(Double totalSavingInPercentage) {
+		this.totalSavingInPercentage = totalSavingInPercentage;
+	}
+
+	@Override
+	public String toString() {
+		return "LabSearchResponse [id=" + id + ", locationName=" + locationName + ", isNABLAccredited="
+				+ isNABLAccredited + ", diagnosticTests=" + diagnosticTests + ", totalCost=" + totalCost
+				+ ", totalCostForPatient=" + totalCostForPatient + ", totalSavingInPercentage="
+				+ totalSavingInPercentage + "]";
+	}
+
+}
--- dpdocter/src/main/java/com/dpdocter/webservices/DiagnosticTestOrderApi.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/DiagnosticTestOrderApi.java
@@ -7,7 +7,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -18,7 +17,6 @@ import org.springframework.stereotype.Component;
 
 import com.dpdocter.beans.DiagnosticTestSamplePickUpSlot;
 import com.dpdocter.beans.OrderDiagnosticTest;
-import com.dpdocter.collections.DiagnosticTestPickUpSlotCollection;
 import com.dpdocter.exceptions.BusinessException;
 import com.dpdocter.exceptions.ServiceError;
 import com.dpdocter.response.LabSearchResponse;
