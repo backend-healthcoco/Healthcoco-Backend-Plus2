6795079ac674f5b71cf71284362f4d126e455df7
Harish_Sendurse <harish.sendurse@healthcoco.com>
2017-11-28 23:41:10 +0530

    HAPPY-2813 - change position of instruction field in Prescription PDF

--- dpdocter/src/main/java/com/dpdocter/services/impl/PatientVisitServiceImpl.java
+++ dpdocter/src/main/java/com/dpdocter/services/impl/PatientVisitServiceImpl.java
@@ -1914,8 +1914,7 @@ public class PatientVisitServiceImpl implements PatientVisitService {
 										showIntructions = true;
 										prescriptionItem.setInstructions(
 												!DPDoctorUtils.anyStringEmpty(prescriptionItem.getInstructions())
-														? "<font size='1'><b>Instruction : </b>"
-																+ prescriptionItem.getInstructions() + "</font>"
+														? "<b>Instruction : </b>" + prescriptionItem.getInstructions()
 														: null);
 									}
 									String duration = "";
