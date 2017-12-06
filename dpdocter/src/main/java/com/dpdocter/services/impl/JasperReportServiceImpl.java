dd0b63a9999b056f92598e7010b15b1ca4473764
Harish_Sendurse <harish.sendurse@healthcoco.com>
2017-12-06 16:39:31 +0530

    increase band size

--- dpdocter/src/main/java/com/dpdocter/services/impl/JasperReportServiceImpl.java
+++ dpdocter/src/main/java/com/dpdocter/services/impl/JasperReportServiceImpl.java
@@ -1591,7 +1591,7 @@ public class JasperReportServiceImpl implements JasperReportService {
 				parameter.get("footerBottomText").toString())) {
 			bandHeight = 85;
 		} else {
-			bandHeight = 25;
+			bandHeight = 45;
 		}
 		int Startwith = 2;
 
