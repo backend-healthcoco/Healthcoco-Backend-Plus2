3d8590513d823db51ab211300b64c289ddfe1fec
Harish_Sendurse <harish.sendurse@healthcoco.com>
2018-01-04 01:43:47 +0530

    HAPPY-2940 resolved issue of patient Search

--- dpdocter/src/main/java/com/dpdocter/elasticsearch/services/impl/ESRegistrationServiceImpl.java
+++ dpdocter/src/main/java/com/dpdocter/elasticsearch/services/impl/ESRegistrationServiceImpl.java
@@ -112,12 +112,12 @@ public class ESRegistrationServiceImpl implements ESRegistrationService {
 		ESPatientResponseDetails patientResponseDetails = null;
 		try {
 			searchTerm = searchTerm.toLowerCase();
-
 			BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder()
 					.must(QueryBuilders.termQuery("locationId", locationId))
 					.must(QueryBuilders.termQuery("hospitalId", hospitalId))
-					.should(QueryBuilders
-							.matchPhraseQuery(AdvancedSearchType.LOCAL_PATIENT_NAME.getSearchType(), searchTerm)
+					.should(QueryBuilders.regexpQuery(AdvancedSearchType.LOCAL_PATIENT_NAME.getSearchType(),
+							searchTerm.replaceAll(".,(?=[]\\[+&|!(){}^\"~*?:\\\\-])", "\\\\").replace(" ", "\\s")
+									+ ".*")
 							.boost(4))
 					.should(QueryBuilders
 							.matchPhrasePrefixQuery(AdvancedSearchType.EMAIL_ADDRESS.getSearchType(), searchTerm)
