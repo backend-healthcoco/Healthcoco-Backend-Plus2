925b554cf9863757c3b893c7f390f28fbadf452d
Harish_Sendurse <harish.sendurse@healthcoco.com>
2018-01-06 13:07:12 +0530

    HAPPY-2956 Backend : dpdocter : LabtoLab : add LabReport

--- dpdocter/src/main/java/com/dpdocter/services/impl/LabReportsServiceImpl.java
+++ dpdocter/src/main/java/com/dpdocter/services/impl/LabReportsServiceImpl.java
@@ -191,14 +191,12 @@ public class LabReportsServiceImpl implements LabReportsService {
 						.findOne(labReportsCollection.getUploadedByLocationId());
 				String message = labReportUploadMessage;
 				SMSTrackDetail smsTrackDetail = new SMSTrackDetail();
-
 				smsTrackDetail.setType("LAB REPORT UPLOAD");
 				SMSDetail smsDetail = new SMSDetail();
 				smsDetail.setUserId(daughterlocationCollection.getId());
 				SMS sms = new SMS();
 				smsDetail.setUserName(daughterlocationCollection.getLocationName());
-				// message = message.replace("{patientName}",
-				// labTestSampleCollection.getPatientName());
+				message = message.replace("{patientName}", request.getPatientName());
 				message = message.replace("{specimenName}", labTestSampleCollection.getSampleType());
 				message = message.replace("{parentLab}", parentLocationCollection.getLocationName());
 				System.out.println(message);
