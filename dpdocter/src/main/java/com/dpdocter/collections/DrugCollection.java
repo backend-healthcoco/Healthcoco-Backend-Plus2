018fe0c0beaa645b6eb9a1e5d2305e484411bbb2
Harish_Sendurse <harish.sendurse@healthcoco.com>
2018-01-15 16:45:06 +0530

    resolved issue in prescription

--- dpdocter/src/main/java/com/dpdocter/collections/DrugCollection.java
+++ dpdocter/src/main/java/com/dpdocter/collections/DrugCollection.java
@@ -102,17 +102,6 @@ public class DrugCollection extends GenericCollection {
 	@Field
 	private Long inventoryQuantity;
 
-	@Field
-	private String instructions;
-
-	public String getInstructions() {
-		return instructions;
-	}
-
-	public void setInstructions(String instructions) {
-		this.instructions = instructions;
-	}
-
 	public ObjectId getId() {
 		return id;
 	}
