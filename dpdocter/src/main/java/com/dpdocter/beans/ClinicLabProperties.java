8fa5054f7e937b3c95d8295c0e62cd71f927bc3b
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-01-18 12:07:55 +0530

    Changes for clinic types in clinic

--- dpdocter/src/main/java/com/dpdocter/beans/ClinicLabProperties.java
+++ dpdocter/src/main/java/com/dpdocter/beans/ClinicLabProperties.java
@@ -5,69 +5,100 @@ import org.codehaus.jackson.map.annotate.JsonSerialize;
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class ClinicLabProperties {
 
-    private String id;
+	private String id;
 
-    private Boolean isClinic = true;
+	private Boolean isClinic = true;
 
-    private Boolean isLab = false;
+	private Boolean isLab = false;
 
-    private Boolean isOnlineReportsAvailable = false;
+	private Boolean isParent = false;
 
-    private Boolean isNABLAccredited = false;
+	private Boolean isDentalWorksLab = false;
 
-    private Boolean isHomeServiceAvailable = false;
+	private Boolean isDentalImagingLab = false;
 
-    public String getId() {
-	return id;
-    }
+	private Boolean isOnlineReportsAvailable = false;
 
-    public void setId(String id) {
-	this.id = id;
-    }
+	private Boolean isNABLAccredited = false;
 
-    public Boolean getIsLab() {
-	return isLab;
-    }
+	private Boolean isHomeServiceAvailable = false;
 
-    public void setIsLab(Boolean isLab) {
-	this.isLab = isLab;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public Boolean getIsOnlineReportsAvailable() {
-	return isOnlineReportsAvailable;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setIsOnlineReportsAvailable(Boolean isOnlineReportsAvailable) {
-	this.isOnlineReportsAvailable = isOnlineReportsAvailable;
-    }
+	public Boolean getIsLab() {
+		return isLab;
+	}
 
-    public Boolean getIsNABLAccredited() {
-	return isNABLAccredited;
-    }
+	public void setIsLab(Boolean isLab) {
+		this.isLab = isLab;
+	}
 
-    public void setIsNABLAccredited(Boolean isNABLAccredited) {
-	this.isNABLAccredited = isNABLAccredited;
-    }
+	public Boolean getIsOnlineReportsAvailable() {
+		return isOnlineReportsAvailable;
+	}
 
-    public Boolean getIsHomeServiceAvailable() {
-	return isHomeServiceAvailable;
-    }
+	public void setIsOnlineReportsAvailable(Boolean isOnlineReportsAvailable) {
+		this.isOnlineReportsAvailable = isOnlineReportsAvailable;
+	}
 
-    public void setIsHomeServiceAvailable(Boolean isHomeServiceAvailable) {
-	this.isHomeServiceAvailable = isHomeServiceAvailable;
-    }
+	public Boolean getIsNABLAccredited() {
+		return isNABLAccredited;
+	}
 
-    public Boolean getIsClinic() {
-	return isClinic;
-    }
+	public void setIsNABLAccredited(Boolean isNABLAccredited) {
+		this.isNABLAccredited = isNABLAccredited;
+	}
 
-    public void setIsClinic(Boolean isClinic) {
-	this.isClinic = isClinic;
-    }
+	public Boolean getIsHomeServiceAvailable() {
+		return isHomeServiceAvailable;
+	}
 
-    @Override
-    public String toString() {
-	return "ClinicLabProperties [id=" + id + ", isClinic=" + isClinic + ", isLab=" + isLab + ", isOnlineReportsAvailable=" + isOnlineReportsAvailable
-		+ ", isNABLAccredited=" + isNABLAccredited + ", isHomeServiceAvailable=" + isHomeServiceAvailable + "]";
-    }
+	public void setIsHomeServiceAvailable(Boolean isHomeServiceAvailable) {
+		this.isHomeServiceAvailable = isHomeServiceAvailable;
+	}
+
+	public Boolean getIsClinic() {
+		return isClinic;
+	}
+
+	public void setIsClinic(Boolean isClinic) {
+		this.isClinic = isClinic;
+	}
+
+	public Boolean getIsParent() {
+		return isParent;
+	}
+
+	public void setIsParent(Boolean isParent) {
+		this.isParent = isParent;
+	}
+
+	public Boolean getIsDentalWorksLab() {
+		return isDentalWorksLab;
+	}
+
+	public void setIsDentalWorksLab(Boolean isDentalWorksLab) {
+		this.isDentalWorksLab = isDentalWorksLab;
+	}
+
+	public Boolean getIsDentalImagingLab() {
+		return isDentalImagingLab;
+	}
+
+	public void setIsDentalImagingLab(Boolean isDentalImagingLab) {
+		this.isDentalImagingLab = isDentalImagingLab;
+	}
+
+	@Override
+	public String toString() {
+		return "ClinicLabProperties [id=" + id + ", isClinic=" + isClinic + ", isLab=" + isLab
+				+ ", isOnlineReportsAvailable=" + isOnlineReportsAvailable + ", isNABLAccredited=" + isNABLAccredited
+				+ ", isHomeServiceAvailable=" + isHomeServiceAvailable + "]";
+	}
 }
