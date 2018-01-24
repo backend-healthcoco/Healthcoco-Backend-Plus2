05336e8a5fc04a4baed240e5a470b4d647377594
Harish_Sendurse <harish.sendurse@healthcoco.com>
2018-01-24 13:36:57 +0530

    HAPPY-3056 Android:HealthcocoPad: Visit: Need to Get Visits for ALL doctors

--- dpdocter/src/main/java/com/dpdocter/webservices/PatientVisitApi.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/PatientVisitApi.java
@@ -136,6 +136,9 @@ public class PatientVisitApi {
 			throw new BusinessException(ServiceError.InvalidInput,
 					"Patient Id, Hospital Id, Location Id Cannot Be Empty");
 		}
+		if (doctorId.equalsIgnoreCase("null")) {
+			doctorId = null;
+		}
 		List<PatientVisitResponse> patienVisitResponse = patientVisitService.getVisit(doctorId, locationId, hospitalId,
 				patientId, page, size, otpService.checkOTPVerified(doctorId, locationId, hospitalId, patientId),
 				updatedTime, visitFor);
