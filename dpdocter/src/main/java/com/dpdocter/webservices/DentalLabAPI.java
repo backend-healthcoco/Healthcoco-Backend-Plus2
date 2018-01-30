a91bacdf0e3c2423d815fe7b1e282b6dbdddcda1
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-01-30 19:14:16 +0530

    changes in change status api

--- dpdocter/src/main/java/com/dpdocter/webservices/DentalLabAPI.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/DentalLabAPI.java
@@ -293,13 +293,13 @@ public class DentalLabAPI {
 	@GET
 	@ApiOperation(value = PathProxy.DentalLabUrls.CHANGE_REQUEST_STATUS, notes = PathProxy.DentalLabUrls.CHANGE_REQUEST_STATUS)
 	public Response<Boolean> getCBListByParentLab(@QueryParam("requestId") String requestId,
-			@QueryParam("status") String status) {
+			@QueryParam("status") String status , @QueryParam("isCollectedAtLab") Boolean isCollectedAtLab) {
 		if (status == null) {
 			logger.warn("Invalid Input");
 			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
 		}
 		Response<Boolean> response = new Response<Boolean>();
-		response.setData(dentalLabService.changeStatus(requestId, status));
+		response.setData(dentalLabService.changeStatus(requestId, status,isCollectedAtLab));
 
 		return response;
 	}
