f29e88b6904922a5e11cdf3a937bbe4a1bf8d526
Harish_Sendurse <harish.sendurse@healthcoco.com>
2018-02-03 01:18:54 +0530

    HAPPY-3055	Backend : dpDocter : Doctor Lab : APIs

--- dpdocter/src/main/java/com/dpdocter/services/DoctorLabService.java
+++ dpdocter/src/main/java/com/dpdocter/services/DoctorLabService.java
@@ -8,7 +8,6 @@ import com.dpdocter.request.DoctorLabDoctorReferenceRequest;
 import com.dpdocter.request.DoctorLabFavouriteDoctorRequest;
 import com.dpdocter.request.DoctorLabReportUploadRequest;
 import com.dpdocter.request.MyFiileRequest;
-import com.dpdocter.response.DoctorLabDoctorReferenceResponse;
 import com.dpdocter.response.DoctorLabFavouriteDoctorResponse;
 import com.dpdocter.response.DoctorLabReportResponse;
 import com.dpdocter.response.DoctorLabSearchDoctorResponse;
