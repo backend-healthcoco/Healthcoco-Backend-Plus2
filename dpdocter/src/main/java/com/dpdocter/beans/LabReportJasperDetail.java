f10600e04ef9cc95373a3e84f421fddd434a7bd1
Harish_Sendurse <harish.sendurse@healthcoco.com>
2018-02-05 23:35:25 +0530

    HAPPY-3055 Backend : dpDocter : Doctor Lab : APIs

---
+++ dpdocter/src/main/java/com/dpdocter/beans/LabReportJasperDetail.java
@@ -0,0 +1,44 @@
+package com.dpdocter.beans;
+
+public class LabReportJasperDetail {
+	private int no;
+
+	private String patientName;
+
+	private String gender;
+
+	private String test;
+
+	public String getPatientName() {
+		return patientName;
+	}
+
+	public void setPatientName(String patientName) {
+		this.patientName = patientName;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+}
