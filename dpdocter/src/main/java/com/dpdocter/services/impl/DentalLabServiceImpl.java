400523cce6e3912180c16872f4345b6a1d5a61e1
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-02-27 14:50:35 +0530

    Dental lab :: bug resolved for dental tooth number

--- dpdocter/src/main/java/com/dpdocter/services/impl/DentalLabServiceImpl.java
+++ dpdocter/src/main/java/com/dpdocter/services/impl/DentalLabServiceImpl.java
@@ -510,6 +510,7 @@ public class DentalLabServiceImpl implements DentalLabService {
 				for (DentalWorksSampleRequest dentalWorksSampleRequest : request.getDentalWorksSamples()) {
 					DentalWorksSample dentalWorksSample = new DentalWorksSample();
 					BeanUtil.map(dentalWorksSampleRequest, dentalWorksSample);
+					dentalWorksSample.setDentalToothNumbers(dentalWorksSampleRequest.getDentalToothNumbers());
 					if (dentalWorksSampleRequest.getDentalStagesForDoctor() != null) {
 						dentalStages = new ArrayList<>();
 						for (DentalStageRequest dentalStageRequest : dentalWorksSampleRequest
@@ -554,6 +555,7 @@ public class DentalLabServiceImpl implements DentalLabService {
 					dentalWorksSampleRequest.setUniqueWorkId(uniqueWorkId);
 					DentalWorksSample dentalWorksSample = new DentalWorksSample();
 					BeanUtil.map(dentalWorksSampleRequest, dentalWorksSample);
+					dentalWorksSample.setDentalToothNumbers(dentalWorksSampleRequest.getDentalToothNumbers());
 					if (dentalWorksSampleRequest.getDentalStagesForDoctor() != null) {
 						dentalStages = new ArrayList<>();
 						for (DentalStageRequest dentalStageRequest : dentalWorksSampleRequest
