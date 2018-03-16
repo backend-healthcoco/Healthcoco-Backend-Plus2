3e6220ff905d1b9380e9813a718b98a36f50c90d
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-03-16 18:35:28 +0530

    Doctor Listing :: handled get doctor issue

--- dpdocter/src/main/java/com/dpdocter/elasticsearch/services/impl/ESAppointmentServiceImpl.java
+++ dpdocter/src/main/java/com/dpdocter/elasticsearch/services/impl/ESAppointmentServiceImpl.java
@@ -719,6 +719,7 @@ public class ESAppointmentServiceImpl implements ESAppointmentService {
 									? doctorDocument.getCountry() + ", " : "")
 							+ (!DPDoctorUtils.anyStringEmpty(doctorDocument.getPostalCode())
 									? doctorDocument.getPostalCode() : "");
+					
 					if (!DPDoctorUtils.anyStringEmpty(address)) {
 						if (address.charAt(address.length() - 2) == ',') {
 							address = address.substring(0, address.length() - 2);
