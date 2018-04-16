60db0e69e91e9695768b8ebd97ebf2b1ba6cc10a
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-04-16 12:48:52 +0530

    Nutrition Goal stamping while crating request

--- dpdocter/src/main/java/com/dpdocter/services/impl/NutritionServiceImpl.java
+++ dpdocter/src/main/java/com/dpdocter/services/impl/NutritionServiceImpl.java
@@ -153,7 +153,7 @@ public class NutritionServiceImpl implements NutritionService{
 				}
 				if(response.getDoctorId() != null)
 				{
-					UserCollection userCollection = userRepository.findOne(new ObjectId(response.getDoctorId()));
+					userCollection = userRepository.findOne(new ObjectId(response.getDoctorId()));
 					response.setDoctorName(userCollection.getFirstName());
 				}
 				
