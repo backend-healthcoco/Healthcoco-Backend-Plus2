ee0ea85720c26060141f6b2bf5b3ce810a016a88
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-05-08 19:17:18 +0530

    changes for multiple image upload Dental imaging

---
+++ dpdocter/src/main/java/com/dpdocter/collections/DentalImagingReportsCollection.java
@@ -0,0 +1,156 @@
+package com.dpdocter.collections;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import com.dpdocter.response.ImageURLResponse;
+
+@Document(collection = "dental_imaging_reports_collection")
+public class DentalImagingReportsCollection extends GenericCollection {
+
+	@Id
+	private ObjectId id;
+
+	@Field
+	private ObjectId patientId;
+
+	@Field
+	private String serviceName;
+
+	@Field
+	private ObjectId doctorId;
+
+	@Field
+	private ObjectId locationId;
+
+	@Field
+	private ImageURLResponse report;
+
+	@Field
+	private ObjectId hospitalId;
+
+	@Field
+	private Boolean discarded = false;
+
+	@Field
+	private ObjectId uploadedByDoctorId;
+
+	@Field
+	private ObjectId uploadedByLocationId;
+
+	@Field
+	private ObjectId uploadedByHospitalId;
+
+	@Field
+	private Boolean isSharedToPatient = false;
+
+	@Field
+	private ObjectId requestId;
+
+	public ObjectId getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(ObjectId patientId) {
+		this.patientId = patientId;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public ObjectId getDoctorId() {
+		return doctorId;
+	}
+
+	public void setDoctorId(ObjectId doctorId) {
+		this.doctorId = doctorId;
+	}
+
+	public ObjectId getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(ObjectId locationId) {
+		this.locationId = locationId;
+	}
+
+	public ObjectId getHospitalId() {
+		return hospitalId;
+	}
+
+	public void setHospitalId(ObjectId hospitalId) {
+		this.hospitalId = hospitalId;
+	}
+
+	public Boolean getDiscarded() {
+		return discarded;
+	}
+
+	public void setDiscarded(Boolean discarded) {
+		this.discarded = discarded;
+	}
+
+	public ObjectId getUploadedByDoctorId() {
+		return uploadedByDoctorId;
+	}
+
+	public void setUploadedByDoctorId(ObjectId uploadedByDoctorId) {
+		this.uploadedByDoctorId = uploadedByDoctorId;
+	}
+
+	public ObjectId getUploadedByLocationId() {
+		return uploadedByLocationId;
+	}
+
+	public void setUploadedByLocationId(ObjectId uploadedByLocationId) {
+		this.uploadedByLocationId = uploadedByLocationId;
+	}
+
+	public ObjectId getUploadedByHospitalId() {
+		return uploadedByHospitalId;
+	}
+
+	public void setUploadedByHospitalId(ObjectId uploadedByHospitalId) {
+		this.uploadedByHospitalId = uploadedByHospitalId;
+	}
+
+	public Boolean getIsSharedToPatient() {
+		return isSharedToPatient;
+	}
+
+	public void setIsSharedToPatient(Boolean isSharedToPatient) {
+		this.isSharedToPatient = isSharedToPatient;
+	}
+
+	public ObjectId getRequestId() {
+		return requestId;
+	}
+
+	public void setRequestId(ObjectId requestId) {
+		this.requestId = requestId;
+	}
+
+	public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public ImageURLResponse getReport() {
+		return report;
+	}
+
+	public void setReport(ImageURLResponse report) {
+		this.report = report;
+	}
+
+}
---
+++ dpdocter/src/main/java/com/dpdocter/request/DentalImagingReportsAddRequest.java
@@ -0,0 +1,145 @@
+package com.dpdocter.request;
+
+public class DentalImagingReportsAddRequest {
+
+	private String patientId;
+
+	private String doctorName;
+
+	private String mobileNumber;
+
+	private String serviceName;
+
+	private String doctorId;
+
+	private String locationId;
+
+	private String hospitalId;
+
+	private Boolean discarded = false;
+
+	private String uploadedByDoctorId;
+
+	private String uploadedByLocationId;
+
+	private String uploadedByHospitalId;
+
+	private Boolean isSharedToPatient = false;
+
+	private String requestId;
+
+	public String getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+	public String getDoctorName() {
+		return doctorName;
+	}
+
+	public void setDoctorName(String doctorName) {
+		this.doctorName = doctorName;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getServiceName() {
+		return serviceName;
+	}
+
+	public void setServiceName(String serviceName) {
+		this.serviceName = serviceName;
+	}
+
+	public String getDoctorId() {
+		return doctorId;
+	}
+
+	public void setDoctorId(String doctorId) {
+		this.doctorId = doctorId;
+	}
+
+	public String getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(String locationId) {
+		this.locationId = locationId;
+	}
+
+	public String getHospitalId() {
+		return hospitalId;
+	}
+
+	public void setHospitalId(String hospitalId) {
+		this.hospitalId = hospitalId;
+	}
+
+	public Boolean getDiscarded() {
+		return discarded;
+	}
+
+	public void setDiscarded(Boolean discarded) {
+		this.discarded = discarded;
+	}
+
+	public String getUploadedByDoctorId() {
+		return uploadedByDoctorId;
+	}
+
+	public void setUploadedByDoctorId(String uploadedByDoctorId) {
+		this.uploadedByDoctorId = uploadedByDoctorId;
+	}
+
+	public String getUploadedByLocationId() {
+		return uploadedByLocationId;
+	}
+
+	public void setUploadedByLocationId(String uploadedByLocationId) {
+		this.uploadedByLocationId = uploadedByLocationId;
+	}
+
+	public String getUploadedByHospitalId() {
+		return uploadedByHospitalId;
+	}
+
+	public void setUploadedByHospitalId(String uploadedByHospitalId) {
+		this.uploadedByHospitalId = uploadedByHospitalId;
+	}
+
+	public Boolean getIsSharedToPatient() {
+		return isSharedToPatient;
+	}
+
+	public void setIsSharedToPatient(Boolean isSharedToPatient) {
+		this.isSharedToPatient = isSharedToPatient;
+	}
+
+	public String getRequestId() {
+		return requestId;
+	}
+
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+
+	@Override
+	public String toString() {
+		return "DentalImagingReportsAddRequest [patientId=" + patientId + ", doctorName=" + doctorName
+				+ ", mobileNumber=" + mobileNumber + ", serviceName=" + serviceName + ", doctorId=" + doctorId
+				+ ", locationId=" + locationId + ", hospitalId=" + hospitalId + ", discarded=" + discarded
+				+ ", uploadedByDoctorId=" + uploadedByDoctorId + ", uploadedByLocationId=" + uploadedByLocationId
+				+ ", uploadedByHospitalId=" + uploadedByHospitalId + ", isSharedToPatient=" + isSharedToPatient
+				+ ", requestId=" + requestId + "]";
+	}
+
+}
