0f84e19fba772cfe6ff0cf9aad40b6ee06a86123
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-05-16 11:30:23 +0530

    Dental billing :: beans for challan generator

---
+++ dpdocter/src/main/java/com/dpdocter/beans/DentalWorksInvoice.java
@@ -0,0 +1,210 @@
+package com.dpdocter.beans;
+
+import java.util.Date;
+import java.util.List;
+
+public class DentalWorksInvoice {
+
+	private String id;
+
+	private String doctorId;
+
+	private String locationId;
+
+	private String hospitalId;
+
+	private String patientId;
+
+	private String uploadedByDoctorId;
+
+	private String uploadedByLocationId;
+
+	private String uploadedByHospitalId;
+	
+	private String uniqueInvoiceId;
+
+	private Discount totalDiscount;
+
+	private double totalCost = 0.0;
+
+	private Tax totalTax;
+
+	private Double grandTotal = 0.0;
+
+	private Double oldGrantTotal = this.grandTotal;
+	
+	private Double usedAdvanceAmount = 0.0;
+
+	private Double refundAmount = 0.0;
+
+	private Double balanceAmount = 0.0;
+
+	private Boolean discarded = false;
+
+	private List<String> receiptIds;
+
+	private Date invoiceDate;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDoctorId() {
+		return doctorId;
+	}
+
+	public void setDoctorId(String doctorId) {
+		this.doctorId = doctorId;
+	}
+
+	public String getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(String locationId) {
+		this.locationId = locationId;
+	}
+
+	public String getHospitalId() {
+		return hospitalId;
+	}
+
+	public void setHospitalId(String hospitalId) {
+		this.hospitalId = hospitalId;
+	}
+
+	public String getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+	public String getUploadedByDoctorId() {
+		return uploadedByDoctorId;
+	}
+
+	public void setUploadedByDoctorId(String uploadedByDoctorId) {
+		this.uploadedByDoctorId = uploadedByDoctorId;
+	}
+
+	public String getUploadedByLocationId() {
+		return uploadedByLocationId;
+	}
+
+	public void setUploadedByLocationId(String uploadedByLocationId) {
+		this.uploadedByLocationId = uploadedByLocationId;
+	}
+
+	public String getUploadedByHospitalId() {
+		return uploadedByHospitalId;
+	}
+
+	public void setUploadedByHospitalId(String uploadedByHospitalId) {
+		this.uploadedByHospitalId = uploadedByHospitalId;
+	}
+
+	public String getUniqueInvoiceId() {
+		return uniqueInvoiceId;
+	}
+
+	public void setUniqueInvoiceId(String uniqueInvoiceId) {
+		this.uniqueInvoiceId = uniqueInvoiceId;
+	}
+
+	public Discount getTotalDiscount() {
+		return totalDiscount;
+	}
+
+	public void setTotalDiscount(Discount totalDiscount) {
+		this.totalDiscount = totalDiscount;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	public Tax getTotalTax() {
+		return totalTax;
+	}
+
+	public void setTotalTax(Tax totalTax) {
+		this.totalTax = totalTax;
+	}
+
+	public Double getGrandTotal() {
+		return grandTotal;
+	}
+
+	public void setGrandTotal(Double grandTotal) {
+		this.grandTotal = grandTotal;
+	}
+
+	public Double getOldGrantTotal() {
+		return oldGrantTotal;
+	}
+
+	public void setOldGrantTotal(Double oldGrantTotal) {
+		this.oldGrantTotal = oldGrantTotal;
+	}
+
+	public Double getUsedAdvanceAmount() {
+		return usedAdvanceAmount;
+	}
+
+	public void setUsedAdvanceAmount(Double usedAdvanceAmount) {
+		this.usedAdvanceAmount = usedAdvanceAmount;
+	}
+
+	public Double getRefundAmount() {
+		return refundAmount;
+	}
+
+	public void setRefundAmount(Double refundAmount) {
+		this.refundAmount = refundAmount;
+	}
+
+	public Double getBalanceAmount() {
+		return balanceAmount;
+	}
+
+	public void setBalanceAmount(Double balanceAmount) {
+		this.balanceAmount = balanceAmount;
+	}
+
+	public Boolean getDiscarded() {
+		return discarded;
+	}
+
+	public void setDiscarded(Boolean discarded) {
+		this.discarded = discarded;
+	}
+
+	public List<String> getReceiptIds() {
+		return receiptIds;
+	}
+
+	public void setReceiptIds(List<String> receiptIds) {
+		this.receiptIds = receiptIds;
+	}
+
+	public Date getInvoiceDate() {
+		return invoiceDate;
+	}
+
+	public void setInvoiceDate(Date invoiceDate) {
+		this.invoiceDate = invoiceDate;
+	}
+	
+	
+	
+}
