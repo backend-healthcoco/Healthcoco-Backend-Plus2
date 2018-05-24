f8da4efc8a2b8962a35388e12f0c77744369ab3f
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-05-24 12:16:10 +0530

    Tax beans & collection for dental work billing

---
+++ dpdocter/src/main/java/com/dpdocter/request/AddEditTaxRequest.java
@@ -0,0 +1,77 @@
+package com.dpdocter.request;
+
+public class AddEditTaxRequest {
+
+	private String id;
+
+	private String doctorId;
+
+	private String locationId;
+
+	private String hospitalId;
+
+	private String taxName;
+
+	private String note;
+
+	private Double percentage;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDoctorId() {
+		return doctorId;
+	}
+
+	public void setDoctorId(String doctorId) {
+		this.doctorId = doctorId;
+	}
+
+	public String getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(String locationId) {
+		this.locationId = locationId;
+	}
+
+	public String getHospitalId() {
+		return hospitalId;
+	}
+
+	public void setHospitalId(String hospitalId) {
+		this.hospitalId = hospitalId;
+	}
+
+	public String getTaxName() {
+		return taxName;
+	}
+
+	public void setTaxName(String taxName) {
+		this.taxName = taxName;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public Double getPercentage() {
+		return percentage;
+	}
+
+	public void setPercentage(Double percentage) {
+		this.percentage = percentage;
+	}
+	
+	
+	
+}
