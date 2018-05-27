75e59a37012f281f9f6faafd4669c44e2cd7be90
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-05-27 20:10:58 +0530

    Dental BIlling :: mongo query issue resolve

--- dpdocter/src/main/java/com/dpdocter/repository/DentalWorksAmountRepository.java
+++ dpdocter/src/main/java/com/dpdocter/repository/DentalWorksAmountRepository.java
@@ -1,14 +1,15 @@
 package com.dpdocter.repository;
 
 import org.bson.types.ObjectId;
-import org.springframework.data.elasticsearch.annotations.Query;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.dpdocter.collections.DentalWorksAmountCollection;
 
 public interface DentalWorksAmountRepository  extends MongoRepository<DentalWorksAmountCollection, ObjectId>{
 
 	@Query("{'doctorId' :?0 , 'locationId' :?1 , 'hospitalId' :?2 , 'dentalLabLocationId' : ?3 , 'dentalLabHospitalId' :?4}")
-	public DentalWorksAmountCollection getByDoctorDentalLabIds(ObjectId doctorId, ObjectId locationId,ObjectId hospitalId,ObjectId dentalLabLocationId,ObjectId dentalLabHospitalId);
+	public DentalWorksAmountCollection findByDoctorIdLocationIdHospitalIdDentalLabLocationIdDentalLabHospitalId(ObjectId doctorId, ObjectId locationId,ObjectId hospitalId,ObjectId dentalLabLocationId,ObjectId dentalLabHospitalId);
 
 }
