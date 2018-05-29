f3fd9f9f9dc4fc7428327464b97d7bfa6028bf18
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-05-29 15:11:33 +0530

    dental Billing :: changes for get invoices list

--- dpdocter/src/main/java/com/dpdocter/webservices/DentalLabAPI.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/DentalLabAPI.java
@@ -547,7 +547,7 @@ public class DentalLabAPI {
 	@GET
 	@ApiOperation(value = PathProxy.DentalLabUrls.GET_INVOICES, notes = PathProxy.DentalLabUrls.GET_INVOICES)
 	public Response<DentalWorksInvoice> getInvoices(@QueryParam ("doctorId")String doctorId, @QueryParam ("locationId")String locationId,@QueryParam ("hospitalId") String hospitalId,
-			@QueryParam ("dentalLabLocationId")String dentalLabLocationId, @QueryParam ("dentalLabHospitalId")String dentalLabHospitalId, @QueryParam ("from")Long from, @QueryParam ("to")Long to, @QueryParam ("searchTerm")String searchTerm, @QueryParam ("size") int size,
+			@QueryParam ("dentalLabLocationId")String dentalLabLocationId, @QueryParam ("dentalLabHospitalId")String dentalLabHospitalId,@DefaultValue("0") @QueryParam ("from")Long from, @QueryParam ("to")Long to, @QueryParam ("searchTerm")String searchTerm, @QueryParam ("size") int size,
 			@QueryParam ("page")int page) {
 		if (DPDoctorUtils.allStringsEmpty(doctorId,locationId,hospitalId,dentalLabHospitalId,dentalLabLocationId)) {
 			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
