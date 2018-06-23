1ff824d7ef882d458486ccb27150b48999d3fb0d
Harish_Sendurse <harish.sendurse@healthcoco.com>
2018-06-23 18:42:49 +0530

    HAPPY-3379 Backend : Doctor : Create API for Koisk Dynamic UI

--- dpdocter/src/main/java/com/dpdocter/collections/KioskDynamicUiCollection.java
+++ dpdocter/src/main/java/com/dpdocter/collections/KioskDynamicUiCollection.java
@@ -6,7 +6,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collection = "koisk_dynamic_ui_Cl")
+@Document(collection = "koisk_dynamic_ui_cl")
 public class KioskDynamicUiCollection extends GenericCollection {
 	@Field
 	private ObjectId id;
