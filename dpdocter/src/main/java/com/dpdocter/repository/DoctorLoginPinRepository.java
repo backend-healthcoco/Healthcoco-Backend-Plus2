abcc523e3fc6ab951c6bce2ad649072baa4a0c72
Harish_Sendurse <harish.sendurse@healthcoco.com>
2018-07-13 13:17:18 +0530

    resolved issue in kiosk pin API

--- dpdocter/src/main/java/com/dpdocter/repository/DoctorLoginPinRepository.java
+++ dpdocter/src/main/java/com/dpdocter/repository/DoctorLoginPinRepository.java
@@ -1,8 +1,9 @@
 package com.dpdocter.repository;
 
 import org.bson.types.ObjectId;
-import org.springframework.data.elasticsearch.annotations.Query;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dpdocter.collections.DoctorLoginPinCollection;
