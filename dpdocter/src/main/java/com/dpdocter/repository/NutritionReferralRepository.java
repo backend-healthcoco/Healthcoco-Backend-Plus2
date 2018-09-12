61d1c75293253b929ba1ae9e9c1a67a232bf81f7
harish <harish.sendurse@healthcoco.com>
2018-09-12 13:13:05 +0530

    HAPPY-3728 Backend : Doctor :: AssessmentForm : API Implemention

--- dpdocter/src/main/java/com/dpdocter/repository/NutritionReferralRepository.java
+++ dpdocter/src/main/java/com/dpdocter/repository/NutritionReferralRepository.java
@@ -3,7 +3,7 @@ package com.dpdocter.repository;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.dpdocter.elasticsearch.repository.NutritionReferralCollection;
+import com.dpdocter.collections.NutritionReferralCollection;
 
 public interface NutritionReferralRepository extends MongoRepository<NutritionReferralCollection, ObjectId> {
 
