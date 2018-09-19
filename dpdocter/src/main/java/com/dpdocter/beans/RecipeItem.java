deb2d0a130f1889469ae9ba286abe32f3707fad7
harish <harish.sendurse@healthcoco.com>
2018-09-19 18:23:51 +0530

    HAPPY-3724 Backend : Doctor :: RECIPE: add edit API for Nutrient , Ingredient, Recipe

--- dpdocter/src/main/java/com/dpdocter/beans/RecipeItem.java
+++ dpdocter/src/main/java/com/dpdocter/beans/RecipeItem.java
@@ -9,32 +9,37 @@ public class RecipeItem {
 	private String name;
 	private double value;
 	private QuantityEnum type;
+
 	public ObjectId getId() {
 		return id;
 	}
+
 	public void setId(ObjectId id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-		public void setValue(int value) {
-		this.value = value;
-	}
+
 	public QuantityEnum getType() {
 		return type;
 	}
+
 	public void setType(QuantityEnum type) {
 		this.type = type;
 	}
+
 	public double getValue() {
 		return value;
 	}
+
 	public void setValue(double value) {
 		this.value = value;
 	}
-	
+
 }
