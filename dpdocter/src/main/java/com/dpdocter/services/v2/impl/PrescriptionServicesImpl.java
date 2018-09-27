8ba1553d5b65649534bfba8c3c7d31180ebf386b
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-09-27 16:05:59 +0530

    HAPPY-3453 Drug dosage duration direction not updating issue resolve

--- dpdocter/src/main/java/com/dpdocter/services/v2/impl/PrescriptionServicesImpl.java
+++ dpdocter/src/main/java/com/dpdocter/services/v2/impl/PrescriptionServicesImpl.java
@@ -399,6 +399,23 @@ public class PrescriptionServicesImpl implements PrescriptionServices {
 			prescriptions = aggregationResults.getMappedResults();
 			if (prescriptions != null && !prescriptions.isEmpty()) {
 				for (Prescription prescription : prescriptions) {
+					if(prescription.getItems() != null && !prescription.getItems().isEmpty())
+					{
+						for(PrescriptionItemDetail itemDetail : prescription.getItems())
+						{
+							if (itemDetail.getDrug() != null) {
+								itemDetail.getDrug().setDosage(itemDetail.getDosage());
+								itemDetail.getDrug().setDirection(itemDetail.getDirection());
+								itemDetail.getDrug().setDuration(itemDetail.getDuration());
+							}
+							
+							itemDetail.setDuration(null);
+							itemDetail.setDosage(null);
+							itemDetail.setDirection(null);
+							
+						}
+					}
+					
 					if (prescription.getTests() != null && !prescription.getTests().isEmpty()) {
 						List<TestAndRecordDataResponse> diagnosticTests = new ArrayList<TestAndRecordDataResponse>();
 						for (TestAndRecordData data : prescription.getTests()) {
@@ -516,6 +533,21 @@ public class PrescriptionServicesImpl implements PrescriptionServices {
 
 			if (prescriptions != null && !prescriptions.isEmpty()) {
 				prescription = prescriptions.get(0);
+				
+				if (prescription.getItems() != null && !prescription.getItems().isEmpty()) {
+					for (PrescriptionItemDetail itemDetail : prescription.getItems()) {
+						if (itemDetail.getDrug() != null) {
+							itemDetail.getDrug().setDosage(itemDetail.getDosage());
+							itemDetail.getDrug().setDirection(itemDetail.getDirection());
+							itemDetail.getDrug().setDuration(itemDetail.getDuration());
+						}
+
+						itemDetail.setDuration(null);
+						itemDetail.setDosage(null);
+						itemDetail.setDirection(null);
+
+					}
+				}
 				if (prescription.getTests() != null && !prescription.getTests().isEmpty()) {
 					List<TestAndRecordDataResponse> diagnosticTests = new ArrayList<TestAndRecordDataResponse>();
 					for (TestAndRecordData data : prescription.getTests()) {
