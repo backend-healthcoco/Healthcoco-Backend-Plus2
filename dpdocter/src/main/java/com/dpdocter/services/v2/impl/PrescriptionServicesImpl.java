3e4e6b26e5c775829bf813f54074229528e461ee
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-11-10 20:47:04 +0530

    /HAPPY-3543 Get list drug structure update

--- dpdocter/src/main/java/com/dpdocter/services/v2/impl/PrescriptionServicesImpl.java
+++ dpdocter/src/main/java/com/dpdocter/services/v2/impl/PrescriptionServicesImpl.java
@@ -276,21 +276,25 @@ public class PrescriptionServicesImpl implements PrescriptionServices {
 
 				for (Prescription prescription : prescriptions) {
 
-					if (prescription.getItems() != null && !prescription.getItems().isEmpty()) {
-						for (PrescriptionItemDetail itemDetail : prescription.getItems()) {
+					
+					if(prescription.getItems() != null && !prescription.getItems().isEmpty())
+					{
+						for(PrescriptionItemDetail itemDetail : prescription.getItems())
+						{
 							if (itemDetail.getDrug() != null) {
 								itemDetail.getDrug().setDosage(itemDetail.getDosage());
 								itemDetail.getDrug().setDirection(itemDetail.getDirection());
 								itemDetail.getDrug().setDuration(itemDetail.getDuration());
 							}
 
+							
 							itemDetail.setDuration(null);
 							itemDetail.setDosage(null);
 							itemDetail.setDirection(null);
-
+							
 						}
 					}
-
+					
 					if (prescription.getTests() != null && !prescription.getTests().isEmpty()) {
 						List<TestAndRecordDataResponse> diagnosticTests = new ArrayList<TestAndRecordDataResponse>();
 						for (TestAndRecordData data : prescription.getTests()) {
