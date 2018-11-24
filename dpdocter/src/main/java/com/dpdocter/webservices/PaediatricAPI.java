b638823aa9d3389945cac417e239c16435e07c3c
Parag Pakhale <parag.pakhale@healthcoco.com>
2018-11-24 17:55:43 +0530

    HAPPY-833 Changes for matrix param api to get vaccine brand

--- dpdocter/src/main/java/com/dpdocter/webservices/PaediatricAPI.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/PaediatricAPI.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.MatrixParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -20,6 +21,7 @@ import com.dpdocter.exceptions.BusinessException;
 import com.dpdocter.exceptions.ServiceError;
 import com.dpdocter.request.MultipleVaccineEditRequest;
 import com.dpdocter.request.VaccineRequest;
+import com.dpdocter.response.GroupedVaccineBrandAssociationResponse;
 import com.dpdocter.response.VaccineBrandAssociationResponse;
 import com.dpdocter.response.VaccineResponse;
 import com.dpdocter.services.PaediatricService;
@@ -88,16 +90,29 @@ public class PaediatricAPI {
 	@GET
 	@ApiOperation(value = PathProxy.PaediatricUrls.GET_VACCINE_BRAND_ASSOCIATION, notes = PathProxy.PaediatricUrls.GET_VACCINE_BRAND_ASSOCIATION)
 	public Response<VaccineBrandAssociationResponse> getVaccineBrandAssociation(@QueryParam("vaccineId") String vaccineId, @QueryParam("vaccineBrandId") String vaccineBrandId) {
-		if (DPDoctorUtils.allStringsEmpty(vaccineBrandId,vaccineId)) {
+		/*if (DPDoctorUtils.allStringsEmpty(vaccineBrandId,vaccineId)) {
 			logger.warn("Invalid Input");
 			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
-		}
+		}*/
 		List<VaccineBrandAssociationResponse> vaccineResponse = paediatricService.getVaccineBrandAssociation(vaccineId, vaccineBrandId);
 		Response<VaccineBrandAssociationResponse> response = new Response<VaccineBrandAssociationResponse>();
 		response.setDataList(vaccineResponse);
 		return response;
 	}
-
+	
+	@Path(value = PathProxy.PaediatricUrls.GET_MULTIPLE_VACCINE_BRAND_ASSOCIATION)
+	@GET
+	@ApiOperation(value = PathProxy.PaediatricUrls.GET_MULTIPLE_VACCINE_BRAND_ASSOCIATION, notes = PathProxy.PaediatricUrls.GET_MULTIPLE_VACCINE_BRAND_ASSOCIATION)
+	public Response<GroupedVaccineBrandAssociationResponse> getMultipleVaccineBrandAssociation(@MatrixParam("vaccineId") List<String> vaccineIds, @QueryParam("vaccineBrandId") String vaccineBrandId) {
+		/*if (DPDoctorUtils.allStringsEmpty(vaccineBrandId,vaccineId)) {
+			logger.warn("Invalid Input");
+			throw new BusinessException(ServiceError.InvalidInput, "Invalid Input");
+		}*/
+		List<GroupedVaccineBrandAssociationResponse> vaccineResponse = paediatricService.getGroupedVaccineBrandAssociation(vaccineIds);
+		Response<GroupedVaccineBrandAssociationResponse> response = new Response<GroupedVaccineBrandAssociationResponse>();
+		response.setDataList(vaccineResponse);
+		return response;
+	}
 
 	@Path(value = PathProxy.PaediatricUrls.ADD_EDIT_MULTIPLE_VACCINE)
 	@POST
