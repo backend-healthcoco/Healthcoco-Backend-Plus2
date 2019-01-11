5d7d2954cfcb8289fe4ace414ae69a3271eb1da5
harish <harish.sendurse@healthcoco.com>
2019-01-11 16:36:25 +0530

    HAPPY-3962 Healthcoco : DpDoctor : Analytics of Appointments

---
+++ dpdocter/src/main/java/com/dpdocter/beans/AppointmentAnalyticData.java
@@ -0,0 +1,108 @@
+package com.dpdocter.beans;
+
+import java.util.Date;
+
+import com.dpdocter.enums.AppointmentState;
+
+public class AppointmentAnalyticData {
+	private String id;
+	private String firstName;
+	private String localPatientName;
+	private String appointmentId;
+	private WorkingHours time;
+	private Date fromDate;
+	private Date toDate;
+	private AppointmentState state = AppointmentState.NEW;
+	private String subject;
+	private String explanation;
+	private String doctorName;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLocalPatientName() {
+		return localPatientName;
+	}
+
+	public void setLocalPatientName(String localPatientName) {
+		this.localPatientName = localPatientName;
+	}
+
+	public String getAppointmentId() {
+		return appointmentId;
+	}
+
+	public void setAppointmentId(String appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+
+	public WorkingHours getTime() {
+		return time;
+	}
+
+	public void setTime(WorkingHours time) {
+		this.time = time;
+	}
+
+	public Date getFromDate() {
+		return fromDate;
+	}
+
+	public void setFromDate(Date fromDate) {
+		this.fromDate = fromDate;
+	}
+
+	public Date getToDate() {
+		return toDate;
+	}
+
+	public void setToDate(Date toDate) {
+		this.toDate = toDate;
+	}
+
+	public AppointmentState getState() {
+		return state;
+	}
+
+	public void setState(AppointmentState state) {
+		this.state = state;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getExplanation() {
+		return explanation;
+	}
+
+	public void setExplanation(String explanation) {
+		this.explanation = explanation;
+	}
+
+	public String getDoctorName() {
+		return doctorName;
+	}
+
+	public void setDoctorName(String doctorName) {
+		this.doctorName = doctorName;
+	}
+
+}
