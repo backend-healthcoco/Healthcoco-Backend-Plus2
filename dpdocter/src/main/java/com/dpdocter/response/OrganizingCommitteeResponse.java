b3a72408fe1b494791a283a65c4189b25364ee12
harish <harish.sendurse@healthcoco.com>
2019-01-16 18:58:51 +0530

    HAPPY-4143 Backend : dpdocter :: Doctor Conference API

---
+++ dpdocter/src/main/java/com/dpdocter/response/OrganizingCommitteeResponse.java
@@ -0,0 +1,46 @@
+package com.dpdocter.response;
+
+public class OrganizingCommitteeResponse {
+
+	private String id;
+
+	private String profileImage;
+
+	private String firstName;
+
+	private String role;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getProfileImage() {
+		return profileImage;
+	}
+
+	public void setProfileImage(String profileImage) {
+		this.profileImage = profileImage;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	
+}
---
+++ dpdocter/src/main/java/com/dpdocter/services/ConferenceService.java
@@ -0,0 +1,37 @@
+package com.dpdocter.services;
+
+import java.util.List;
+
+import com.dpdocter.beans.DoctorConference;
+import com.dpdocter.beans.DoctorConferenceAgenda;
+import com.dpdocter.beans.DoctorConferenceSession;
+import com.dpdocter.beans.SessionTopic;
+import com.dpdocter.beans.SpeakerProfile;
+
+public interface ConferenceService {
+
+	public List<SessionTopic> getTopics(int size, int page, boolean discarded, String searchTerm);
+
+	public SessionTopic getTopic(String id);
+
+	public List<SpeakerProfile> getSpeakerProfiles(int size, int page, boolean discarded, String searchTerm);
+
+	public SpeakerProfile getSpeakerProfile(String id);
+
+	public List<DoctorConferenceSession> getConferenceSessions(int size, int page, boolean discarded, String city,
+			Integer fromtime, Integer toTime, String fromDate, String toDate, String searchTerm, String conferenceId,
+			List<String> topics);
+
+	public DoctorConferenceSession getConferenceSession(String id);
+
+	public List<DoctorConference> getDoctorConference(int size, int page, boolean discarded, String city,
+			String speciality, String fromDate, String toDate, String searchTerm);
+
+	public DoctorConference getDoctorConference(String id);
+
+	public List<DoctorConferenceAgenda> getConferenceAgenda(int size, int page, boolean discarded, String fromDate,
+			String toDate, String searchTerm, String conferenceId);
+
+	public DoctorConferenceAgenda getConferenceAgenda(String id);
+
+}
