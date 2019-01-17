06ebe9b8e47ac16aac71c35342674ddbdf4aff23
harish <harish.sendurse@healthcoco.com>
2019-01-17 11:27:59 +0530

    HAPPY-4143 Backend : dpdocter :: Doctor Conference API

--- dpdocter/src/main/java/com/dpdocter/beans/SessionQuestion.java
+++ dpdocter/src/main/java/com/dpdocter/beans/SessionQuestion.java
@@ -59,3 +59,107 @@ public class SessionQuestion extends GenericCollection {
 	}
 
 }
+package com.dpdocter.beans;
+
+import com.dpdocter.collections.GenericCollection;
+
+public class SessionQuestion extends GenericCollection {
+	private String id;
+	private String type = "CONFERENCE_SESSION";
+	private String sessionId;
+	private String userId;
+	private Integer noOfLikes;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Integer getNoOfLikes() {
+		return noOfLikes;
+	}
+
+	public void setNoOfLikes(Integer noOfLikes) {
+		this.noOfLikes = noOfLikes;
+	}
+
+}
+package com.dpdocter.beans;
+
+import com.dpdocter.collections.GenericCollection;
+
+public class SessionQuestion extends GenericCollection {
+	private String id;
+	private String type = "CONFERENCE_SESSION";
+	private String sessionId;
+	private String userId;
+	private Integer noOfLikes;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Integer getNoOfLikes() {
+		return noOfLikes;
+	}
+
+	public void setNoOfLikes(Integer noOfLikes) {
+		this.noOfLikes = noOfLikes;
+	}
+
+}
