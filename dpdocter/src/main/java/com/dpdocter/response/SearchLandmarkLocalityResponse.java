83197c1e04bd0b48124712ab6992a7e945e9d07e
nehakariya <neha.pateliya@healthcoco.com>
2019-03-06 18:02:08 -0500

    HAPPY-4235 Backend : Patient : Web Search: Get landmak and localities API

---
+++ dpdocter/src/main/java/com/dpdocter/response/SearchLandmarkLocalityResponse.java
@@ -0,0 +1,60 @@
+package com.dpdocter.response;
+
+public class SearchLandmarkLocalityResponse {
+
+    private String city;
+
+    private String name;
+
+    private String slugUrl;
+
+    private double latitude;
+
+    private double longitude;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSlugUrl() {
+		return slugUrl;
+	}
+
+	public void setSlugUrl(String slugUrl) {
+		this.slugUrl = slugUrl;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	@Override
+	public String toString() {
+		return "SearchLandmarkLocalityResponse [city=" + city + ", name=" + name + ", slugUrl=" + slugUrl
+				+ ", latitude=" + latitude + ", longitude=" + longitude + "]";
+	}
+}
