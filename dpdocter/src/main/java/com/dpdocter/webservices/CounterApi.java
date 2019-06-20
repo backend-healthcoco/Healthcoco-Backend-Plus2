68f96077b730ab8039b274eda3392d3e8e65f6ce
Harish Sendurse <harish.sendurse@healthcoco.com>
2019-06-20 23:57:54 +0530

    HAPPY-4520 Backend : Doctor :: Procedure Sheet Issue

--- dpdocter/src/main/java/com/dpdocter/webservices/CounterApi.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/CounterApi.java
@@ -293,7 +293,7 @@ public class CounterApi {
 	public Response<MealCounter> getMealCounters(@PathParam("userId") String userId, @QueryParam("size") int size,
 			@QueryParam("page") int page, @QueryParam("fromDate") String fromDate, @QueryParam("toDate") String toDate,
 			@QueryParam("mealTime") String mealTime) {
-		if (DPDoctorUtils.anyStringEmpty(userId, mealTime)) {
+		if (DPDoctorUtils.anyStringEmpty(userId)) {
 			logger.warn("userId,mealtime must not null or empty");
 			throw new BusinessException(ServiceError.InvalidInput, "userId,mealtime must not null or empty");
 
