531e7db07ed2866e5d7c1ffb18364e8eedcac5df
nehakariya <neha.pateliya@healthcoco.com>
2019-07-09 22:07:04 -0400

    HAPPY-4548 Backend : Dpdocter : Add drugs in OT reports and pdf - Added remaining file

--- dpdocter/src/main/java/com/dpdocter/webservices/v2/PatientVisitApi.java
+++ dpdocter/src/main/java/com/dpdocter/webservices/v2/PatientVisitApi.java
@@ -53,7 +53,7 @@ public class PatientVisitApi {
 			@PathParam(value = "locationId") String locationId, @PathParam(value = "hospitalId") String hospitalId,
 			@PathParam(value = "patientId") String patientId, @QueryParam(value = "page") int page,
 			@QueryParam(value = "size") int size, @DefaultValue("0") @QueryParam("updatedTime") String updatedTime,
-			@QueryParam("visitFor") String visitFor) {
+			@QueryParam("visitFor") String visitFor, @DefaultValue("true") @QueryParam("discarded") Boolean discarded) {
 
 		if (DPDoctorUtils.anyStringEmpty(patientId, hospitalId, locationId)) {
 			logger.warn("Patient Id, Doctor Id, Hospital Id, Location Id Cannot Be Empty");
@@ -65,7 +65,7 @@ public class PatientVisitApi {
 		}
 		List<PatientVisitResponse> patienVisitResponse = patientVisitService.getVisit(doctorId, locationId, hospitalId,
 				patientId, page, size, otpService.checkOTPVerified(doctorId, locationId, hospitalId, patientId),
-				updatedTime, visitFor);
+				updatedTime, visitFor, discarded);
 
 		Response<PatientVisitResponse> response = new Response<PatientVisitResponse>();
 		response.setDataList(patienVisitResponse);
