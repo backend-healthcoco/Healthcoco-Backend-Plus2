d62801c212da621243853c9b15354fa17ef7c038
nehakariya <neha.pateliya@healthcoco.com>
2020-01-14 18:35:38 -0500

    HAPPY-5462 - Added missing files

---
+++ dpdocter/src/main/java/com/dpdocter/beans/School.java
@@ -0,0 +1,72 @@
+package com.dpdocter.beans;
+
+import com.dpdocter.collections.GenericCollection;
+import com.dpdocter.enums.RoleEnum;
+
+public class School extends GenericCollection {
+
+	private String id;
+	private String schoolName;
+	private String userName;
+	private String emailAddress;
+	private String userType = RoleEnum.SCHOOL.getRole();
+	private Boolean isVerified = Boolean.FALSE ;
+	private Boolean isActivated = Boolean.FALSE;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public Boolean getIsVerified() {
+		return isVerified;
+	}
+
+	public void setIsVerified(Boolean isVerified) {
+		this.isVerified = isVerified;
+	}
+
+	public Boolean getIsActivated() {
+		return isActivated;
+	}
+
+	public void setIsActivated(Boolean isActivated) {
+		this.isActivated = isActivated;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+}
