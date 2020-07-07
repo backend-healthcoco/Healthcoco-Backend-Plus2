9827944e766bf2f026d2e5d820ff795deb105ad5
Nikita <nikita.patil@healthcoco.com>
2020-07-07 15:05:14 +0530

    HAPPY-5896 one change add

--- dpdocter/src/main/java/com/dpdocter/collections/CountryCollection.java
+++ dpdocter/src/main/java/com/dpdocter/collections/CountryCollection.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import com.dpdocter.beans.Tax;
 import com.dpdocter.enums.PeriodEnums;
 
+@Document(collection = "country_cl")
 public class CountryCollection extends GenericCollection{
 
 	@Id
