9338ed63edd59278689d9c2a7333ab62d85c1844
Nikita <nikita.patil@healthcoco.com>
2020-07-18 09:36:08 +0530

    HAPPY-5906 ignored file added

---
+++ dpdocter/src/main/java/com/dpdocter/response/SubscriptionResponse.java
@@ -0,0 +1,123 @@
+package com.dpdocter.response;
+
+import com.dpdocter.beans.Discount;
+import com.dpdocter.enums.PaymentMode;
+
+public class SubscriptionResponse {
+
+	private String id;
+
+	private String transactionId;
+
+	private String transactionStatus;
+
+	private String doctorId;	
+	
+//	private String razorPayAccountId;
+
+	private Discount discount;
+
+	private Double amount = 0.0;
+
+	private Double discountAmount = 0.0;
+
+	private Double transferAmount = 0.0;
+
+	private PaymentMode mode = PaymentMode.ONLINE;
+
+	//private String chequeNo;
+
+	private String orderId;
+
+	private String reciept;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public String getTransactionStatus() {
+		return transactionStatus;
+	}
+
+	public void setTransactionStatus(String transactionStatus) {
+		this.transactionStatus = transactionStatus;
+	}
+
+	public String getDoctorId() {
+		return doctorId;
+	}
+
+	public void setDoctorId(String doctorId) {
+		this.doctorId = doctorId;
+	}
+
+	public Discount getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(Discount discount) {
+		this.discount = discount;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Double getDiscountAmount() {
+		return discountAmount;
+	}
+
+	public void setDiscountAmount(Double discountAmount) {
+		this.discountAmount = discountAmount;
+	}
+
+	public Double getTransferAmount() {
+		return transferAmount;
+	}
+
+	public void setTransferAmount(Double transferAmount) {
+		this.transferAmount = transferAmount;
+	}
+
+	public PaymentMode getMode() {
+		return mode;
+	}
+
+	public void setMode(PaymentMode mode) {
+		this.mode = mode;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getReciept() {
+		return reciept;
+	}
+
+	public void setReciept(String reciept) {
+		this.reciept = reciept;
+	}
+	
+	
+}
