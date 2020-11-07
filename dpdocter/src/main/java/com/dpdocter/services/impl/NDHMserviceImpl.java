5ba4f45aef787876c3980e3ed6e6cd9fc1709516
Aman Verma <aman.verma@healthcoco.com>
2020-11-07 12:41:31 +0530

    HAPPY-6054

--- dpdocter/src/main/java/com/dpdocter/services/impl/NDHMserviceImpl.java
+++ dpdocter/src/main/java/com/dpdocter/services/impl/NDHMserviceImpl.java
@@ -1,6 +1,11 @@
 package com.dpdocter.services.impl;
 
 import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Base64;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpUriRequest;
@@ -8,6 +13,7 @@ import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -36,21 +42,65 @@ public class NDHMserviceImpl implements NDHMservices{
 		NdhmOauthResponse response=null;
 		try {
 			String url="https://dev.ndhm.gov.in/gateway/v0.5/sessions";
-			ObjectMapper mapper = new ObjectMapper();
-			 ByteArrayOutputStream out = new ByteArrayOutputStream();
-			HttpClient client = HttpClients.custom().build();
-			HttpUriRequest httprequest = RequestBuilder.post().addParameter("clientId",NDHM_CLIENTID)
-					.addParameter("clientSecret", NDHM_CLIENT_SECRET)
-					
-			  .setUri(url)
-			  .setHeader( "Content-Type","application/json")
-			  .build();
-			//System.out.println("response"+client.execute(httprequest));
+			JSONObject orderRequest = new JSONObject();
+			orderRequest.put("clientId",NDHM_CLIENTID);
+			orderRequest.put("clientSecret",  NDHM_CLIENT_SECRET);
+//			ObjectMapper mapper = new ObjectMapper();
+//			 ByteArrayOutputStream out = new ByteArrayOutputStream();
+//			HttpClient client = HttpClients.custom().build();
+//			HttpUriRequest httprequest = RequestBuilder.post(url).addParameter("clientId",NDHM_CLIENTID)
+//					.addParameter("clientSecret", NDHM_CLIENT_SECRET)
+//					
+//			 // .setUri(url)
+//			  .setHeader( "Content-Type","application/json")
+//			  .build();
+//			System.out.println("client"+NDHM_CLIENT_SECRET);
+//			System.out.println("clientId"+NDHM_CLIENTID);
+//			
+//			 org.apache.http.HttpResponse responses = client.execute(httprequest);
+//			 responses.getEntity().writeTo(out);
 			
-			 org.apache.http.HttpResponse responses = client.execute(httprequest);
-			 responses.getEntity().writeTo(out);
-			 response = mapper.readValue(out.toString(),NdhmOauthResponse.class);
-			System.out.println("response"+out.toString());
+			
+			// String authStr=keyId+":"+secret;
+		//	 String authStringEnc = Base64.getEncoder().encodeToString(authStr.getBytes());
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			
+			con.setDoOutput(true);
+			
+			System.out.println(con.getErrorStream());
+			con.setDoInput(true);
+			// optional default is POST
+			con.setRequestMethod("POST");
+//			con.setRequestProperty("User-Agent",
+//					"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+		//	con.setRequestProperty("Accept-Charset", "UTF-8");
+			con.setRequestProperty("Content-Type","application/json");
+		//	con.setRequestProperty("Authorization", "Basic " +  authStringEnc);
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(orderRequest.toString());
+			System.out.println("Orderrequest:"+orderRequest.toString());
+			  wr.flush();
+	            wr.close();
+	            con.disconnect();
+	            InputStream in=con.getInputStream();
+	        //    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				System.out.println(con.getErrorStream());
+				/* response = new StringBuffer(); */
+				StringBuffer output = new StringBuffer();
+				int c = 0;
+				while ((c=in.read()) !=-1) {
+
+					output.append((char) c);
+					
+				}
+				System.out.println("response:"+output.toString());
+				  ObjectMapper mapper = new ObjectMapper();
+			
+			 response = mapper.readValue(output.toString(),NdhmOauthResponse.class);
+			System.out.println("response"+output.toString());
 		}
 		catch (Exception e) {
 			e.printStackTrace();
