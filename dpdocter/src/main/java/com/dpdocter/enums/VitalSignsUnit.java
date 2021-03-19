1290b70b505d18fec9ae3cd5a02906b7e469111f
Nikita <nikita.patil@healthcoco.com>
2021-03-19 14:30:31 +0530

    HAPPY-6767 med hold

--- dpdocter/src/main/java/com/dpdocter/enums/VitalSignsUnit.java
+++ dpdocter/src/main/java/com/dpdocter/enums/VitalSignsUnit.java
@@ -2,7 +2,8 @@ package com.dpdocter.enums;
 
 public enum VitalSignsUnit {
 	PULSE("beats/min"), BREATHING("breaths/min"), BLOODPRESSURE("mmHg"), HEIGHT("cm"), TEMPERATURE("°F"), WEIGHT(
-			"kg"), SPO2("%"), BSA("m²"),BMI("kg/m²"),FIO2("%"),VENTILATION_MODE("VCV"),CVP("mmHg"),URINE("cc"),IBP("mmHg");
+			"kg"), SPO2("%"), BSA("m²"),BMI("kg/m²"),FIO2("%"),VENTILATION_MODE("VCV"),CVP("mmHg"),URINE("cc"),IBP("mmHg"),
+	BLOODSUGAR("mg/dL");
 	
 	private String unit;
 
