64b5c6dfa3ca23d2156f807eb51347c2c22cc975
nehakariya <neha.pateliya@healthcoco.com>
2021-03-27 17:49:12 -0400

    -HAPPY-6169 Adding files

---
+++ dpdocter/src/main/java/com/dpdocter/repository/TestRepository.java
@@ -0,0 +1,11 @@
+//package com.dpdocter.repository;
+//
+//
+//import org.bson.types.ObjectId;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//import com.dpdocter.collections.TestCollection;
+//
+//public interface TestRepository extends MongoRepository<TestCollection, ObjectId>{
+//
+//}
