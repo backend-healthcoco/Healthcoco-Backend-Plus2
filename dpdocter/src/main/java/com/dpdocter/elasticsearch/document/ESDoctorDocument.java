2e90fdcfe5631dfd7791cbe6be3677e7770c8ac8
Aman Verma <aman.verma@healthcoco.com>
2021-03-31 18:13:59 +0530

    HAPPY-6129

--- dpdocter/src/main/java/com/dpdocter/elasticsearch/document/ESDoctorDocument.java
+++ dpdocter/src/main/java/com/dpdocter/elasticsearch/document/ESDoctorDocument.java
@@ -10,6 +10,7 @@ import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.data.elasticsearch.annotations.MultiField;
 
+import com.dpdocter.beans.Achievement;
 import com.dpdocter.beans.AppointmentSlot;
 import com.dpdocter.beans.ConsultationFee;
 import com.dpdocter.beans.DOB;
@@ -165,6 +166,38 @@ public class ESDoctorDocument extends DoctorLocation implements Comparable<ESDoc
 	
 	@Field(type = FieldType.Nested)
 	private List<WorkingSchedule> onlineWorkingSchedules;
+	
+	@Field(type = FieldType.Text)
+	private String RegistrationImageUrl;
+	@Field(type = FieldType.Text)
+	private String RegistrationThumbnailUrl;
+	
+	@Field(type = FieldType.Text)
+	private String photoIdImageUrl;
+	
+	@Field(type = FieldType.Boolean)
+	private Boolean isRegistrationDetailsVerified =false;
+	
+	@Field(type = FieldType.Boolean)
+	private Boolean isPhotoIdVerified =false;
+	
+	
+	@Field(type = FieldType.Text)
+	private String professionalStatement;
+	
+	@Field(type = FieldType.Nested)
+	private List<Achievement> achievements;
+
+	
+	@Field(type = FieldType.Nested)
+	private AppointmentSlot onlineConsultationSlot;
+	
+	@Field(type = FieldType.Boolean)
+	private Boolean isHealthcocoDoctor=false;
+	
+	@Field(type = FieldType.Boolean)
+	private Boolean isExpUpdated=false;
+
 	
 	public String getDoctorSlugURL() {
 		return doctorSlugURL;
@@ -566,6 +599,88 @@ public class ESDoctorDocument extends DoctorLocation implements Comparable<ESDoc
 
 	public void setOnlineWorkingSchedules(List<WorkingSchedule> onlineWorkingSchedules) {
 		this.onlineWorkingSchedules = onlineWorkingSchedules;
+	}
+	
+	
+
+	public String getRegistrationImageUrl() {
+		return RegistrationImageUrl;
+	}
+
+	public void setRegistrationImageUrl(String registrationImageUrl) {
+		RegistrationImageUrl = registrationImageUrl;
+	}
+
+	public String getRegistrationThumbnailUrl() {
+		return RegistrationThumbnailUrl;
+	}
+
+	public void setRegistrationThumbnailUrl(String registrationThumbnailUrl) {
+		RegistrationThumbnailUrl = registrationThumbnailUrl;
+	}
+
+	public String getPhotoIdImageUrl() {
+		return photoIdImageUrl;
+	}
+
+	public void setPhotoIdImageUrl(String photoIdImageUrl) {
+		this.photoIdImageUrl = photoIdImageUrl;
+	}
+
+	public Boolean getIsRegistrationDetailsVerified() {
+		return isRegistrationDetailsVerified;
+	}
+
+	public void setIsRegistrationDetailsVerified(Boolean isRegistrationDetailsVerified) {
+		this.isRegistrationDetailsVerified = isRegistrationDetailsVerified;
+	}
+
+	public Boolean getIsPhotoIdVerified() {
+		return isPhotoIdVerified;
+	}
+
+	public void setIsPhotoIdVerified(Boolean isPhotoIdVerified) {
+		this.isPhotoIdVerified = isPhotoIdVerified;
+	}
+
+	public String getProfessionalStatement() {
+		return professionalStatement;
+	}
+
+	public void setProfessionalStatement(String professionalStatement) {
+		this.professionalStatement = professionalStatement;
+	}
+
+	public List<Achievement> getAchievements() {
+		return achievements;
+	}
+
+	public void setAchievements(List<Achievement> achievements) {
+		this.achievements = achievements;
+	}
+
+	public AppointmentSlot getOnlineConsultationSlot() {
+		return onlineConsultationSlot;
+	}
+
+	public void setOnlineConsultationSlot(AppointmentSlot onlineConsultationSlot) {
+		this.onlineConsultationSlot = onlineConsultationSlot;
+	}
+
+	public Boolean getIsHealthcocoDoctor() {
+		return isHealthcocoDoctor;
+	}
+
+	public void setIsHealthcocoDoctor(Boolean isHealthcocoDoctor) {
+		this.isHealthcocoDoctor = isHealthcocoDoctor;
+	}
+
+	public Boolean getIsExpUpdated() {
+		return isExpUpdated;
+	}
+
+	public void setIsExpUpdated(Boolean isExpUpdated) {
+		this.isExpUpdated = isExpUpdated;
 	}
 
 	@Override
