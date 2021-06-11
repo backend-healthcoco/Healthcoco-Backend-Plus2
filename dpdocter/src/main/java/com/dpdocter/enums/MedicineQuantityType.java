7d34463bc2aaab3838fabd1e458feb15ad156acd
Aman Verma <aman.verma@healthcoco.com>
2021-06-11 16:18:16 +0530

    HAPPY-6144

--- dpdocter/src/main/java/com/dpdocter/enums/MedicineQuantityType.java
+++ dpdocter/src/main/java/com/dpdocter/enums/MedicineQuantityType.java
@@ -2,7 +2,7 @@ package com.dpdocter.enums;
 
 public enum MedicineQuantityType {
 
-	STRIPS("STRIPS"),TABLETS("TABLETS"),BOTTLE("BOTTLE"),SYRUP("SYRUP"),TUBE("TUBE"),INJECTION("INJECTION"),MASK("MASK");
+	STRIPS("STRIPS"),TABLETS("TABLETS"),BOTTLE("BOTTLE"),SYRUP("SYRUP"),TUBE("TUBE"),INJECTION("INJECTION"),GEL("GEL"),CREAM("CREAM");
 	
 	private String type;
 
